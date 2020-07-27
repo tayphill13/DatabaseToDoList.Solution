1dc421c442c9db1299f02b9505114b2ec0a48097
Tristan Emmerson <tristan@stickerslug.com>
2020-07-27 13:53:43 -0700

    update Item.cs file to include ClearAll() dispose method line. Co-authored-by: Tristan Emmerson <tristan@stickerslug.com> Co-authored-by: Taylor Phillips <taylorphillips133@gmail.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -44,6 +44,11 @@ namespace ToDoList.Models
       MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
       cmd.CommandText = @"DELETE FROM items;";
       cmd.ExecuteNonQuery();
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
     }
 
     public static Item Find(int searchId)
